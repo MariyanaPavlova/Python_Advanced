b7f02cfb530a9ff9fc2c84e219cb7d433f04f149
Mariyana Pavlova <94930314+MariyanaPavlova@users.noreply.github.com>
2023-02-22 16:29:00 +0200

    Delete Tuples_and_Sets directory

--- Tuples_and_Sets/1.3.unique_names.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-names = set()
-
-for i in range(n):
-    name = input()
-    names.add(name)
-
-print("\n".join(names))
-
--- Tuples_and_Sets/2.3.periodic_table.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-
-elem = set()
-
-for i in range(n):
-    cur_el = input().split()
-    elem = elem.union(cur_el)
-
-print(*elem, sep="\n")
